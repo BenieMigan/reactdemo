e9fad0b985636170373b46b941747d2b4108c689
vladimir <vladimirhoueto6@gmail.com>
2024-12-07 09:02:37 +0100

    ma page

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,23 @@ function App() {
             <Route path="/livingroom"  element={<Livingroom/>} />
             <Route path="/outdoors"  element={<Outdoors/>} />
 
-            <Route path="/Paginate2"  element={<Paginate2/>} />
+            <Route path="/Paginate2"  element={<Paginate2
+            mytitle1="Consectuer vehicula ab Consectuer vehicula ab"
+            title1="November 21, 2017 / Kitchen, Lifestyle"
+            description1="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that;"
+            mytitle2="Sunt doloremque blandit inven"
+            title2="November 20, 2017 / Living Room"
+            image2="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/sofa-with-pot.jpg" 
+            image3="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic26-free-img-150x150.jpg" 
+            image4="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic20-free-img-150x150.jpg"
+            description2="Nisl vel urna debitis morbi fringilla malesuada maiores optio! Ratione, facilis. Illum accusantium eros sed tristique, cubilia? Vel eu cupiditate. Habitasse urna molestie hendrerit, animi eros"
+            image5="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic04-free-img.jpg" 
+            image6="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic06-free-img.jpg" 
+            image7="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/10/pic07-free-img.jpg" 
+            image8="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/08/pic08-free-img.jpg" 
+            image9="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/06/pic09-free-img.jpg" 
+
+          />} />        
             <Route path="/Paginate3"  element={<Paginate3/>} />
             
           </Routes>
