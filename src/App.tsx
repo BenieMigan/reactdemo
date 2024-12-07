ed06a95ea9fe920d3588b9d7d7d8a269102e2601
Your Name <you@example.com>
2024-12-07 04:31:49 +0100

    page next établie

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Outdoors from './Components/Navbar/Outdoors';
 import Navbar from './Components/Navbar';
 import Paginate2 from './Components/Body/Paginates/Paginate2';
 import Paginate3 from './Components/Body/Paginates/Paginate3';
+import Nextpaginate from './Components/Body/Paginates/Nextpaginate';
 
 function App() {
   return (
@@ -22,7 +23,25 @@ function App() {
 
             <Route path="/Paginate2"  element={<Paginate2/>} />
             <Route path="/Paginate3"  element={<Paginate3/>} />
-            
+
+            <Route path="/Nextpaginate"  element={<Nextpaginate
+            mytitle1="Consectuer vehicula ab Consectuer vehicula ab"
+            title1="November 21, 2017 / Kitchen, Lifestyle"
+            description1="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that;"
+            mytitle2="Sunt doloremque blandit inven"
+            title2="November 20, 2017 / Living Room"
+            image2="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/sofa-with-pot.jpg" 
+            image3="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic26-free-img-150x150.jpg" 
+            image4="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic20-free-img-150x150.jpg"
+            description2="Nisl vel urna debitis morbi fringilla malesuada maiores optio! Ratione, facilis. Illum accusantium eros sed tristique, cubilia? Vel eu cupiditate. Habitasse urna molestie hendrerit, animi eros"
+            image5="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic04-free-img.jpg" 
+            image6="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic06-free-img.jpg" 
+            image7="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/10/pic07-free-img.jpg" 
+            image8="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/08/pic08-free-img.jpg" 
+            image9="https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/06/pic09-free-img.jpg" 
+
+          />} />
+
           </Routes>
         </Router> 
     </div>
